8762338cdf0d27b3d0e3cf88222639e41e2159ae
PolinaTsugankova <pp6056512@gmail.com>
2023-03-20 15:26:14 +0200

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,20 +1,26 @@
 // Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
-// Під час відправлення форми сторінка не повинна перезавантажуватися.
-// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// Під час відправлення форми сторінка не повинна перезавантажуватися. +
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені. +
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
 const formEl = document.querySelector(".login-form");
 const btnSubmit = document.querySelector('button[type="submit"]');
-const email = document.querySelector('input[type="email"]');
-const password = document.querySelector('input[type="password"]');
+
+formEl.addEventListener("submit", onSubmitBtn);
 
 function onSubmitBtn(event) {
-   event.preventDefault;
+   event.preventDefault();
+   // console.dir(event);
+
+   const email = event.currentTarget.elements.email.value;
+   console.log(email);
+   const password = event.currentTarget.elements.password.value;
+   const userData = { email, password };
 
    if (email.value === "" || password.value === "") {
       alert("Заповність усі поля");
+   } else {
+      console.log(userData);
    }
 }
-
-btnSubmit.addEventListener("submit", onSubmitBtn);
